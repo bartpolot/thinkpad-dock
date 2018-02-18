e1f7068a223aed1d876bdd1fffedda9085160bd5
Bart Polot <bart.polot+gnunet@gmail.com>
2018-02-18 17:58:10 +0100

    Do not install udev rules as executable

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo install -D udev-rules/*.rules   /etc/udev/rules.d/
+sudo install -D udev-rules/*.rules   /etc/udev/rules.d/     -m 640
 sudo install -D src/sys/*.sh         /usr/local/sbin/
 install -d                           "$HOME/local/bin/"
 install -D src/bin/*.sh              "$HOME/local/bin/"
