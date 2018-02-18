15a72f0875726eab3c117f96b45e5b97afe4bcff
Bart Polot <bart.polot+gnunet@gmail.com>
2018-02-18 17:56:24 +0100

    Create directories on install

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@
 
 sudo install -D udev-rules/*.rules   /etc/udev/rules.d/
 sudo install -D src/sys/*.sh         /usr/local/sbin/
-install -D src/bin/*.sh              ~/local/bin/
+install -d                           "$HOME/local/bin/"
+install -D src/bin/*.sh              "$HOME/local/bin/"
+install -d                           "$HOME/.config/dock/"
 install -D src/dock/*                "$HOME/.config/dock/" 
